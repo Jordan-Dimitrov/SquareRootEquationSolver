ae2616e37c612f8a27904cbbf052543befc266d7
Jordan-Dimitrov <dani27game@gmail.com>
2022-07-08 10:51:09 +0300

    forms

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.input = new System.Windows.Forms.TextBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(325, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Equation solver";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(325, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Required format:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(329, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "+ 1a^2 + 1b + 1c";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(335, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "enter equation:";
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(338, 170);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(93, 22);
+            this.input.TabIndex = 4;
+            this.input.TextChanged += new System.EventHandler(this.input_TextChanged);
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(314, 213);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(136, 23);
+            this.btn.TabIndex = 5;
+            this.btn.Text = "run program";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(362, 255);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "result:";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(362, 287);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(0, 17);
+            this.result.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void input_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            string value = input.Text.ToString();
+            string[] tokens = value.Split(' ');
+            string action1 = tokens[0];
+            string first = tokens[1];
+            string action2 = tokens[2];
+            string second = tokens[3];
+            string action3 = tokens[4];
+            string third = tokens[5];
+            string[] firs = first.Split('x');
+            string[] seco = second.Split('x');
+            int a = int.Parse(firs[0]);
+            int b = int.Parse(seco[0]);
+            if (action1=="-")
+            {
+                a = -a;
+            }
+            if (action2=="-")
+            {
+                b = -b;
+            }
+            int c = int.Parse(third);
+            if (action3 == "-")
+            {
+                c = -c;
+            }
+            int d = b * b - 4 * a * c;
+
+            if (d>0)
+            {
+                double sum1 = -b + Math.Sqrt(d);
+                sum1 = sum1 / 2;
+
+                double sum2 = -b - Math.Sqrt(d);
+                sum2 = sum2 / 2;
+
+                result.Text = ($"x1 = {sum1} x2 = {sum2}");
+
+            }
+            else if (d==0)
+            {
+                int temp = a * 2;
+                double sum = -b / temp;
+                result.Text = ($"x1=x2={sum}");
+            }
+            else if (d<0)
+            {
+                result.Text = "no solution!";
+            }
+        }
+    }
+}
